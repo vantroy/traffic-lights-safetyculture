7da09461480486b0a9b4ce0b00fe8ca5dbcae963
vantroy <vantroy@gmail.com>
2016-03-20 01:15:17 -0300

    WIP checkpoint: added some tests, resurrected Crossroads concept with a better name. Tidy'd up. Done away with Class ES6 concept for added practice in old school JS

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 "use strict";
 
 var TrafficLight = require("./lib/traffic-light");
-var constants = require("./lib/constants")
-var CrossRoad = require("./lib/crossroad");
+var constants = require("./lib/constants");
+var LinkedLightsManager = require("./lib/linked-lights-manager");
 
-var cross = new CrossRoad();
+var cross = new LinkedLightsManager();
 
 var lightN = new TrafficLight("North", constants.GREEN, cross);
 var lightS = new TrafficLight("South", constants.GREEN, cross);
@@ -12,10 +12,10 @@ var lightW = new TrafficLight("West", constants.RED, cross);
 var lightE = new TrafficLight("East", constants.RED, cross);
 
 // for (var i = 0; i < 4; i++) {
-//     light.cycle();
+//     light._cycle();
 // }
 
-// lightN.state();
+// lightN._state();
 
 cross.emit("change-yellow");
 
--- test/traffic-light.js
+++ test/traffic-light.js
@@ -1,18 +1,61 @@
+"use strict";
+
 var expect = require("chai").expect;
 var TrafficLight = require("../lib/traffic-light");
-var CrossRoad = require("../lib/crossroad");
+var LinkedLightsManager = require("../lib/linked-lights-manager");
+// var EventEmitter = require("events").EventEmitter;
 
-describe("the TrafficLight class", function () {
+describe("in the TrafficLight class", function () {
 
+    var crossRoad = new LinkedLightsManager();
+    var trafficLight = new TrafficLight("test", "RED", crossRoad);
 
-    describe("#selectColor method", function () {
-        it("returns a colored string", function () {
-            var crossRoad = new CrossRoad();
-            var trafficLight = new TrafficLight("test", crossRoad, "RED");
-            res = trafficLight.selectColor("RED");
-            expect(res).to.equal("RED");
+    describe("#_selectColor should", function () {
+        
+        it("return a colored string containing RED", function () {
+            let res = trafficLight._selectColor("RED");
+            expect(res.indexOf('RED') > -1).to.be.ok;
+        });
+        
+        it("return a colored string containing GREEN", function () {
+            let res = trafficLight._selectColor("GREEN");
+            expect(res.indexOf('GREEN') > -1).to.be.ok;
+        })
+
+        it("return a colored string containing YELLOW", function () {
+            let res = trafficLight._selectColor("YELLOW");
+            expect(res.indexOf('YELLOW') > -1).to.be.ok;
+        })
+        
+    })
+    
+    describe("#_printChanged should", function () {
+        it("print a helpful string", function () {
+            let res = trafficLight.name;
+            expect(res.indexOf("test changed to " == 0)).to.be.ok;
+        })        
+    })
+    
+    describe("#_register should", function () {
+        it("contain 1 registered listeners when init lit stat is NOT green", function () {
+            let trafficLight = new TrafficLight("test", "RED", new LinkedLightsManager());
+            let numLights = trafficLight.linkedLightsManager.listenerCount("change-lights");
+            let numYellow = trafficLight.linkedLightsManager.listenerCount("change-yellow");
+            expect(numLights).to.equal(1);
+            expect(numYellow).to.equal(0);
+        });
+
+        it("contain 2 registered listeners when init lit stat IS green", function () {
+            let trafficLight = new TrafficLight("test", "RED", new LinkedLightsManager());
+            let numLights = trafficLight.linkedLightsManager.listenerCount("change-lights");
+            let numYellow = trafficLight.linkedLightsManager.listenerCount("change-yellow");
+            expect(numLights).to.equal(1);
+            expect(numYellow).to.equal(0);
         })
     })
+    
+    // describe("#_cycle")
+    
 });
 
 
