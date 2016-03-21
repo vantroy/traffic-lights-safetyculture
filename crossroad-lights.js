fbdb8264985c777de3f353f647d738ce0b6bf9bd
vantroy <vantroy@gmail.com>
2016-03-20 23:54:04 -0300

    Adding JSDocs

--- crossroad-lights.js
+++ crossroad-lights.js
@@ -5,33 +5,57 @@ var timeUtil = require("./lib/time-util");
 var logger = require("./config/logger");
 var dateFormat = require('dateformat');
 
-// Start time of simulation
+/**
+ *  This file is just a template to showcase the logic
+ *  of {@link LinkedLightsManager} and {@link TrafficLight}
+ *  It can be easily adapted to simulate traffic lights in real
+ *  time or output a json object with changes, for example.
+ */
+
+
+/**
+ * Start time of simulation as now */
 var startTime = new Date().getTime();
 
-// Duration == 30 minutes as per exercise request
+/** Duration == 30 minutes as per exercise request
+ * duration must be in seconds
+ */
 var duration = 30 * 60;
 
-// Create our 'crossroad' manager
-// Since exercise asks only for (NS) and (EW), a single LinkedLightsManager will suffice
+/**
+ * Create our 'crossroad' manager
+ * Since exercise asks only for (NS) and (EW), a single LinkedLightsManager will suffice
+ * @type {LinkedLightsManager}
+ */
 var crossRoad = new LinkedLightsManager();
 
-// Add the member traffic lights. Choosing to arbitrarily starting off (NS) as GREEN first
-// Since it is a single manager, it is enough that the linked sets have the same
-// initial status, opposed from the concurrent set.
+/**
+ * Add the member traffic lights. Choosing to arbitrarily starting off (NS) as GREEN first
+ * Since it is a single manager, it is enough that the linked sets have the same
+ * initial status, opposed from the concurrent set.
+ */
 crossRoad.addTrafficLight("North", constants.GREEN, checkError);
 crossRoad.addTrafficLight("South", constants.GREEN, checkError);
 crossRoad.addTrafficLight("West", constants.RED, checkError);
 crossRoad.addTrafficLight("East", constants.RED, checkError);
 
-// Timeset in which events will occur
+/**
+ * Timeset in which events will occur. In a real world application a timer
+ * would be used to fire the {@link LinkedLightsManager#tick};
+ */
 var timeSlices;
 timeUtil.sliceTime(startTime, duration, slices => {
     timeSlices = slices;
     checkError(timeSlices);
 });
 
+/**
+ * Simulation starter.
+ * Once again, in a real world application, it should carry a timer instead of
+ * an array with predefined times for {@link LinkedLightsManager#tick} to be called.
+ * @param timesSlices
+ */
 function start(timesSlices) {
-
     crossRoad.on('light-changed-colored', (name, color) => {
         logger.info(`${name} changed to ${color}`)
     });    
@@ -40,7 +64,6 @@ function start(timesSlices) {
         logger.info(`=====> At: ${dateFormat(timesSlices[i], "h:MM:ss TT")}`);
         crossRoad.tick(checkError);
     }
-
 }
 
 // Fire it off
@@ -50,7 +73,12 @@ crossRoad.lights.forEach(function (light) {
 });
 start(timeSlices);
 
-// We have no room for mistakes here
+/**
+ * Let it exit with non zero status in case of error.
+ * All points of failure are properly logged
+ * 
+ * @param err
+ */
 function checkError(err) {
     if(err instanceof Error) {
         log.fatal('Something went horribly wrong. Exiting simulation', err);
